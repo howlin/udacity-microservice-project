2d94972d49742f9a1f8e378b405c76656ea3ffa5
Pádraig Howlin <padraig@howlin.ie>
2020-05-12 10:21:39 +0100

    chore: kuberneties - setup canary builds for backend-feed

--- udacity-c3-restapi-feed/src/server.ts
+++ udacity-c3-restapi-feed/src/server.ts
@@ -22,6 +22,7 @@ const c = config.dev;
   app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    res.header("Canary-Env-Value", process.env.ENV_VALUE);
     next();
   });
 
